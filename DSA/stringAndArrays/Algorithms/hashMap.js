a67ab085c364fede5fbc546f13695ea1863809ff
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2024-11-17 13:37:29 +0530

    commit-it-to-merge

---
+++ DSA/stringAndArrays/Algorithms/hashMap.js
@@ -0,0 +1,13 @@
+// Map() -
+// The Map() constructor creates Map objects.
+// Method to create a map object.
+console.log(new Map()); 
+// Iterables in a map object
+let mapIterable1 = new Map([[ 1, 'one'],[ 2, 'two' ]]);
+let mapIterable2 = new Map(Object.entries({1: 'one', 2: 'two'}));
+console.log(mapIterable1.get(1), mapIterable1.get(2));
+console.log(mapIterable2.get(1), mapIterable2.get(2));
+
+
+
+
