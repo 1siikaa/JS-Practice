3f0324ae4f04c5858d7b1779093d997659cc352b
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2025-05-06 12:53:53 +0530

    binary tree

--- DSA/Binary Tree/Tree Creation/binaryTree.js
+++ DSA/Binary Tree/Tree Creation/binaryTree.js
@@ -1,57 +1,55 @@
-// creating a tree node
-
+// Creating a tree node
 class Node {
-  constructor (value){
-    this.data = value;
-    this.left = null;
-    this.right = null;
+    constructor(value) {
+      this.data = value;
+      this.left = null;
+      this.right = null;
+    }
   }
-}
-let obj = new Node(12);
-console.log(obj);
-
-
-// creating a binary tree using new nodes and joining them by reference
-class binaryTree {
- constructor () {
-    this.root = null;
- }
-
- insertRootNode (value) {
-   const newNode = new Node(value);
-   if(!this.root){
-    this.root = newNode;
-   }
-   else {
-    this._insertNode(this.root, newNode);
-   }
- }
-
- _insertNode (node, newNode) {
-   if(newNode.data < node.data){
-     if(!node.left){
-        node.left = newNode;
-     }
-     else{
-        this._insertNode(node.left, newNode);
-     }
-   }
-   else{
-    this._insertNode (node.right, newNode);
-   }
- }
-}
-
-const tree = new binaryTree();
-tree.insertRootNode(50);
-tree.insertRootNode(30);
-tree.insertRootNode(70);
-tree.insertRootNode(20);
-tree.insertRootNode(40);
-tree.insertRootNode(60);
-tree.insertRootNode(80);
-console.log(tree)
-
-
-
-
+  
+  // Creating a binary tree using new nodes and joining them by reference
+  class BinaryTree {
+    constructor() {
+      this.root = null;
+    }
+  
+    insertRootNode(value) {
+      const newNode = new Node(value);
+      if (!this.root) {
+        this.root = newNode;
+      } else {
+        this._insertNode(this.root, newNode);
+      }
+    }
+  
+    _insertNode(node, newNode) {
+      if (newNode.data < node.data) {
+        if (!node.left) {
+          node.left = newNode;
+        } else {
+          this._insertNode(node.left, newNode);
+        }
+      } else {
+        if (!node.right) {
+          node.right = newNode;
+        } else {
+          this._insertNode(node.right, newNode);
+        }
+      }
+    }
+  }
+  
+  // Usage
+  const obj = new Node(12);
+  console.log("Single node:", obj);
+  
+  const tree = new BinaryTree();
+  tree.insertRootNode(50);
+  tree.insertRootNode(30);
+  tree.insertRootNode(70);
+  tree.insertRootNode(20);
+  tree.insertRootNode(40);
+  tree.insertRootNode(60);
+  tree.insertRootNode(80);
+  
+  console.log("Binary Tree:", JSON.stringify(tree, null, 2));
